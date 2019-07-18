4929f6bd90030f69242087ae2b2612ef505cb21c
holic5839 <holic5839@gmail.com>
2019-07-19 00:18:16 +0900

    선형 합동법을 통한 일양 난수 구하기

--- chapter-02/slkawoon.koo/main.js
+++ chapter-02/slkawoon.koo/main.js
@@ -1,1 +1,11 @@
-const main => () => console.log('hello chapter 02');
+// X[n] = (A*X[n-1] + C) % M
+const randomNumber = (A, C, M) => {
+  var rndnum = [13];
+  for(var i = 0; i < 100; i++) {
+    rndnum = (A*rndnum + C) % M;
+    console.log(rndnum);
+  }
+  return rndnum;
+};
+
+randomNumber(109, 1021, 32768);
