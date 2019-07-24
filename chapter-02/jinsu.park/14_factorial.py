452a92c1bedea7911c1a430423c39f7494e2d4ca
coolgary825 <coolgary825@gmail.com>
2019-07-24 13:25:54 +0000

    고고

---
+++ chapter-02/jinsu.park/14_factorial.py
@@ -0,0 +1,17 @@
+def factorialBase(n):
+    result = 1
+    for i in range(n, 0, -1):
+        result *= i
+        print(i, end=" ")
+        print("")
+        print(result, end=" ")
+
+
+def factorialRecursive(n):
+    if(n == 0 or n ==1):
+        return 1;
+    return n * factorialRecursive(n-1)
+
+factorialBase(5)
+
+factorialRecursive(5)
