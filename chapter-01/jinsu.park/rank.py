28d696437d1c5003f4075634c31af081beb701ef
coolgary825 <coolgary825@gmail.com>
2019-07-24 13:27:10 +0000

    1장 고고

--- chapter-01/jinsu.park/rank.py
+++ chapter-01/jinsu.park/rank.py
@@ -3,14 +3,17 @@ num = 10
 a = [56, 25, 67, 88, 100, 61, 55, 67, 76, 56]
 rank = [0 for _ in range(10)]
 
+count = 0
+
 for i in range(num):
     rank[i] = 1
-
     for j in range(num):
+        count += 1
         if a[j] > a[i]:
             rank[i] += 1  # 파이썬은 후위연산자 ++ 안되네
+            # print(str(i) + str(j))
 
-print("점수 순위")
+print("점수 순위 " + str(count))
 
 for i in range(num):
     print(a[i], rank[i])
