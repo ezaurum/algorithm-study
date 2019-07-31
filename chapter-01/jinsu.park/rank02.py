2fd5ace458cfedfb56028ab4024fe604c1dcd529
박진수 <coolgary@kolon.com>
2019-07-31 13:56:26 +0900

    pre code modify code convention

--- chapter-01/jinsu.park/rank02.py
+++ chapter-01/jinsu.park/rank02.py
@@ -3,7 +3,7 @@ max = 100
 min = 0
 
 a = [56, 25, 67, 88, 100, 61, 55, 67, 76, 56]
-rank = [0 for _ in range(max+2)]
+rank = [0 for _ in range(max + 2)]
 count = 0
 
 for i in range(num):  # 각 점수를 index로 하는 배열 요소 + 1
@@ -13,8 +13,8 @@ for i in range(num):  # 각 점수를 index로 하는 배열 요소 + 1
 rank[max + 1] = 1
 for i in range(max, 0, -1):
     count += 1
-    rank[i] = rank[i]+rank[i+1]
+    rank[i] = rank[i] + rank[i + 1]
 
 print("점수 순위 " + str(count))
 for i in range(num):
-    print(a[i], rank[a[i]+1])
+    print(a[i], rank[a[i] + 1])
