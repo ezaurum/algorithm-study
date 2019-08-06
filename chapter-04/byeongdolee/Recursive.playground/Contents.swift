f54216289bba04add618943a03f1638cd8935380
Byeongdo Lee <bdbdbdlee@gmail.com>
2019-08-06 18:07:19 +0900

    추가

--- chapter-04/byeongdolee/Recursive.playground/Contents.swift
+++ chapter-04/byeongdolee/Recursive.playground/Contents.swift
@@ -1,5 +1,107 @@
 import Foundation
 
+var arr = [45,73,12,44,32,94,78,39,29,56,22,1,6,2]
+func quickSort(_ left: Int, _ right: Int) -> [Int] {
+    let leftValue = arr[left]
+    if left < right {
+        var i = left + 1
+        var j = right
+        while true {
+            while arr[i] < leftValue { i += 1 }
+            while arr[j] > leftValue { j -= 1 }
+            if i >= j {
+                break
+            }
+            
+            let tmp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tmp
+            print(arr)
+        }
+        arr[left] = arr[j]
+        arr[j] = leftValue
+        quickSort(left, j - 1)
+        quickSort(j + 1, right)
+    }
+    
+    return arr
+}
+
+print(quickSort(0, arr.count - 1))
+
+func hanoi(_ n: Int, _ a: String, _ b: String, _ c: String) {
+    if n > 0 {
+        hanoi(n - 1, a, c, b)
+        print("\(n)번 원반을 \(a)에서 \(b)로 이동")
+        hanoi(n - 1, c, b, a)
+    }
+}
+
+hanoi(5, "a", "b", "c")
+
+//P1
+//겉에 감싸기(1) + 앞, 뒤 낱개로 추가(2), 맨 마지막 낱개는 앞 뒤 추가하는게 중복임
+// f(1) = 1
+// f(x) = 3 * f(x-1) - 1
+func parentheses(_ n: Int) -> [String] {
+    if n < 0 {
+        fatalError()
+    }
+    
+    if n == 0 {
+        return []
+    }
+    
+    if n == 1 {
+        return ["()"]
+    }
+    
+    let arr = parentheses(n - 1)
+    var result = [String]()
+    
+    for i in 0..<arr.count {
+        result.append("(\(arr[i]))")
+        
+        let pre = "()\(arr[i])"
+        let post = "\(arr[i])()"
+        result.append(pre)
+        if pre != post {
+            result.append(post)
+        }
+    }
+    
+    return result
+}
+
+for i in 1...5 {
+    let result = parentheses(i)
+    print(result)
+    print(result.count)
+}
+
+
+let max = 4
+
+var arr = Array<Int>(1...max)
+//PERMUTATION
+func permutation(_ n: Int) {
+    
+    if n < max - 1 {
+        for i in n..<max {
+            let tmp = arr[i]
+            arr[i] = arr[n]
+            arr[n] = tmp
+            permutation(n+1)
+            arr[n] = arr[i]
+            arr[i] = tmp
+        }
+    } else {
+        print(arr)
+    }
+}
+
+permutation(0)
+
 // FACTORIAL
 func factorial(_ n: Int) -> Int {
     if n < 0 {
@@ -24,10 +126,10 @@ func factorial2(_ n: Int) -> Int {
     return result
 }
 
-for i in 1..<20 {
-    print(factorial(i))
-    log(Double(factorial(i)))
-}
+//for i in 1..<20 {
+//    print(factorial(i))
+//    log(Double(factorial(i)))
+//}
 
 // FIBONACCI
 func fibonacci(_ n: Int) -> Int {
@@ -84,8 +186,8 @@ func gcd2(_ m: Int, _ n: Int) -> Int {
     return gcd2(n, m % n)
 }
 
-gcd(24, 18)
-gcd(128, 72)
+//gcd(24, 18)
+//gcd(128, 72)
 
 
 
