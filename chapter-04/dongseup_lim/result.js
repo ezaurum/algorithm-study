c61fdbc9da6ea248e191a1cc031defebdb40f0a4
dongseup <dongseup83@gmail.com>
2019-08-08 09:32:43 +0900

    알고리즘 연습

--- chapter-04/dongseup_lim/result.js
+++ chapter-04/dongseup_lim/result.js
@@ -1,17 +1,17 @@
 // 4장 피보나치(재귀)
 function fib(num) {
-    if (num == 1 || num == 2) {
-        return num;
-    } else {
-        return fib(num - 1) + fib(num - 2);
-    }
+  if (num == 1 || num == 2) {
+      return num;
+  } else {
+      return fib(num - 1) + fib(num - 2);
   }
-  
-  for (let n = 1; n <= 20; n++) {
-    console.log(`${n} : ${fib(n)}`);
-  }
+}
 
-console.log('-------------------------------------------');
+for (let n = 1; n <= 20; n++) {
+  console.log(`${n} : ${fib(n)}`);
+}
+
+//////////////////////////////////////////////////////////////////////
 
 
 function fib2(num) {
@@ -26,5 +26,33 @@ function fib2(num) {
 
 console.log(fib2(10)); 
 
-console.log('-------------------------------------------');
+//////////////////////////////////////////////////////////////////////
 
+// codility 1 - binaryGap
+
+function solution(N) { 
+  var number = N; 
+  var binary = '';
+  var counter = -1;
+  var max = 0;
+
+  while(number > 0) {
+    var digit = number % 2;
+    
+    if(digit === 1) {
+      if (counter > max) {
+        max = counter;
+      }
+      counter = 0;
+    } else if(counter >= 0) {
+      counter++;
+    }
+
+    binary = '' + digit + binary
+    number = parseInt(number / 2);
+  }
+  console.log('number, binary:', number, binary);
+  return max
+}
+
+solution(10);
